d5417a5ad4cf444fa6804a8bf040d24872c9d4ba
Akraja712 <ashokkumar7hitter@gmail.com>
2024-08-30 15:21:33 +0530

    sidebar.blade.php

--- staffs/resources/views/layouts/partials/sidebar.blade.php
+++ staffs/resources/views/layouts/partials/sidebar.blade.php
@@ -16,8 +16,8 @@
                 <img src="https://www.gravatar.com/avatar/" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-            <a href="#" class="d-block">{{ explode(' ', auth()->staffs()->getFullname())[0] }}</a>
-            </div>
+        <a href="#" class="d-block"><?php echo e(explode(' ', auth()->user()->name)[0]); ?></a>
+    </div>
         </div>
 
         <!-- Sidebar Menu -->
